29531f52819012c2c72702c6ae3d7bf8a5978e93
Rebwar Hosein Poori <Rebwar.poori@gmail.com>
2020-01-21 23:57:54 +0330

    Add DataProvider

--- XUnitTest.Test/OddEvenTest.cs
+++ XUnitTest.Test/OddEvenTest.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Reflection;
 using System.Text;
 using Xunit;
+using Xunit.Sdk;
 using XUnitTest.Model;
 
 namespace XUnitTest.Test
@@ -16,6 +18,26 @@ namespace XUnitTest.Test
             OddEven even = new OddEven();
             even.EvenCheck(number);
             Assert.Equal(isEven, even.IsEven);
+        }
+    }
+    public class MyDataAttribute : DataAttribute
+    {
+        public override IEnumerable<object[]> GetData(MethodInfo testMethod)
+        {
+            List<object[]> data = new List<object[]>();
+            data.Add(new object[] { true, 2 });
+            data.Add(new object[] { false, 1 });
+            data.Add(new object[] { true, 4 });
+            return data;
+        }
+
+        [Theory]
+        [MyData]
+        public void EvenTest01(bool iseven,int number)
+        {
+            OddEven even = new OddEven();
+            even.EvenCheck(number);
+            Assert.Equal(iseven, even.IsEven);
         }
     }
 }
