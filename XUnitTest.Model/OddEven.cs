351582d03e5994892a25af50f30637c35f395339
Rebwar Hosein Poori <Rebwar.poori@gmail.com>
2020-01-21 23:30:07 +0330

    Add inlineData to reduce Method implemantion

---
+++ XUnitTest.Model/OddEven.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace XUnitTest.Model
+{
+   public class OddEven
+    {
+        public bool IsOdd { get; set; }
+        public bool IsEven { get; set; }
+        public void EvenCheck(int number)
+        {
+            if (number % 2 == 0)
+            {
+                IsEven = true;
+            }
+        }
+        public void OddCheck(int number)
+        {
+            if (number % 2 != 0)
+            {
+                IsOdd = true;
+            }
+        }
+
+    }
+}
---
+++ XUnitTest.Test/OddEvenTest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+using XUnitTest.Model;
+
+namespace XUnitTest.Test
+{
+    public class OddEvenTest
+    {
+        [Theory]
+        [InlineData(true,2)]
+        [InlineData(false,1)]
+        public void EvenTest(bool isEven,int number)
+        {
+            OddEven even = new OddEven();
+            even.EvenCheck(number);
+            Assert.Equal(isEven, even.IsEven);
+        }
+    }
+}
